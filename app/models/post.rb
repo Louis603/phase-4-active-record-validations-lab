a2f8dea817d71b1791d1213c803a4929b13e3dd9
Louis Y <louisy603@hotmail.com>
2022-06-07 13:24:53 -0400

    commit tree 59a78821134797617119473b200d760a5b17b6b9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']  }
+
+    validate :title_is_clickbait
+
+    # def title_is_clickbait
+    #     if title != "Won't Believe" "Secret" "Top [number]" "Guess"
+    #         errors.add(:title, "must include")
+    #     end
+    # end
+
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+
+    def title_is_clickbait
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+      end
 end
